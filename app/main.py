7bf9554a3c77760ed10962554e2acc393bfa2195
unknown <guilherme@socialdome.com.br>
2022-08-15 22:01:34 -0300

    Criação da database

--- app/main.py
+++ app/main.py
@@ -4,14 +4,21 @@ import mysql.connector
 
 app = FastAPI()
 
-@app.get("/api/v1/database_status")
-async def database_status():
+def connect_to_database():
     mydb = mysql.connector.connect(
     host="mariadb",
     user="reikinoapp",
     password="reikinoapp"
     # database="reikinoapp"
     )
+    return mydb
+
+@app.post("/api/v1/user/create_user")
+async def create_user(first_name: str, last_name: str, email: str, password: str, is_active: bool):
+    mydb = connect_to_database()
     mycursor = mydb.cursor()
-    mycursor.execute("SHOW DATABASES")
-    return {'databases': mycursor.fetchall()}
+    query = "INSERT INTO reikinoapp.user (first_name, last_name, email, password, is_active) VALUES (%s, %s, %s, %s, %s)"
+    values = (first_name, last_name, email, password, is_active)
+    mycursor.execute(query, values)
+    mydb.commit()
+    return {'message': 'dale gremio'}
