c4a06539c1d766d86d850a020ad3a45ce65ae4ae
Naan Andrade <naan.santos.andrade@gmail.com>
2022-09-01 19:52:06 -0300

    mutchos bugs

--- Dockerfile
+++ Dockerfile
@@ -4,21 +4,11 @@ LABEL maintainer="Instituto Xavier"
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt ./tmp/requirements.txt
-COPY ./requirements.dev.txt /tmp/requirements.dev.txt
 COPY ./app /app
 WORKDIR /app
-EXPOSE 8000
 
-ARG DEV=false
 RUN python -m venv /py && \
     /py/bin/pip install --upgrade pip && \
-    /py/bin/pip install -r /tmp/requirements.txt && \
-    if [ $DEV = "true" ]; \
-        then /py/bin/pip install -r /tmp/requirements.dev.txt; \
-    fi && \
-    rm -rf /tmp && \
-    adduser --disabled-password --no-create-home app-user
+    /py/bin/pip install -r /tmp/requirements.txt
 
 ENV PATH="/py/bin:$PATH"
-
-USER app-user
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from uuid import uuid4
+<<<<<<< Updated upstream
 from fastapi import FastAPI
 from models import User, Testimony
 from typing import List
@@ -32,3 +33,84 @@ async def fetch_users():
 @app.post("/api/v1/users")
 async def register_user(user: User):
     database.append(user)
+=======
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
+from typing import Optional
+import mysql.connector
+
+app = FastAPI()
+
+users = {
+    1: {
+        "firt_name": "Naan",
+        "last_name": "Rei Delas",
+        "email": "naan@gmail.com",
+        "password": "12fsfe320423",
+        "is_active": "true"
+    },
+    2: {
+        "firt_name": "Felipe",
+        "last_name": "Cabeça de pica",
+        "email": "felipe@gmail.bom",
+        "password": "senha",
+        "is_active": "true"
+    }
+}
+
+class User(BaseModel):
+    firt_name: str
+    last_name: str
+    email: str
+    password: str
+    is_active: bool
+
+@app.get("/get-user/{user_id}")
+def get_user(user_id : int = Path(None, description = "Digite o ID do usuário que você deseja visualizar.")):
+    if user_id not in users:
+        return {"Mensagem" : "Usuário não existente"}
+    return users[user_id]
+
+@app.post("/create-user/{user_id}")
+def create_user(user_id : int , user : User = Path(None, description = "Digite o ID do usuário que você vai criar. Abaixo no body coloque as informações do usuário.")):
+    if user_id in users: 
+        return {"Erro": "Usuário já existente."}
+    
+    users[user_id] = user
+    return user[user_id]
+
+@app.delete("/delete-user/{user_id}")
+def delete_user(user_id : int):
+    if user_id not in users:
+        return {"Erro" : "Usuário não existente."}
+
+    del users[user_id]
+    return {"Mensagem" : "Usuário deletado"}
+
+
+
+# def connect_to_database():
+#    mydb = mysql.connector.connect(
+#    host="mariadb",
+#    user="reikinoapp",
+#    password="reikinoapp"
+#    # database="reikinoapp"
+#    )
+#    return mydb
+
+# @app.post("/api/v1/user/create_user")
+# async def create_user(
+#    first_name: str, 
+#    last_name: str, 
+#    email: str, 
+#    password: str, 
+#    is_active: bool
+#    ):
+#    mydb = connect_to_database()
+#    mycursor = mydb.cursor()
+#    query = "INSERT INTO reikinoapp.user (first_name, last_name, email, password, is_active) VALUES (%s, %s, %s, %s, %s)"
+#    values = (first_name, last_name, email, password, is_active)
+#    mycursor.execute(query, values)
+#    mydb.commit()
+#    return {'message': 'Usuário Criado'}
+>>>>>>> Stashed changes
